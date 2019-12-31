d80c61203ff0aa249217ec75a2c6bbf00d9f6fcf
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-31 19:48:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -191,9 +191,9 @@ def player_numbers(team_name)
   player_numbers = []
 
   game_information.each do |team|
-    if team["team_name"] == team_name then
-      team[:players].each {|player| player_numbers.push(player[:number])}
-    end
+    #if team["team_name"] == team_name then
+    #  team[:players].each {|player| player_numbers.push(player[:number])}
+    #end
   end
 
   return player_numbers
