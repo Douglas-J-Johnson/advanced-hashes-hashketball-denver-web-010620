221d4eb15ee14f94aff2dc0ddf559e83b08e9857
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-31 17:13:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,16 @@
 def game_hash
-  game_information = {}
+  game_information = {
+	   :home => {
+       :team_name => "Brooklyn Nets",
+       :colors => ["Black", "White"],
+       :players => []
+     },
+     :away => {
+       :team_name => "Charlotte Hornets",
+       :colors => ["Turquoise","Purple"],
+       :players => []       
+     }
+  }
 
   return game_information
 end
