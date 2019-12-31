1b11454362e228c9c1c80581a3d37215d98a722d
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-31 17:21:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,15 +6,15 @@ def game_hash
        :team_name => "Brooklyn Nets",
        :colors => ["Black", "White"],
        :players => [{
-         	:player_name => "string",
-          :number => integer,
-          :shoe => integer,
-          :points => integer,
-          :rebounds => integer,
-          :assists => integer,
-          :steals => integer,
-          :blocks => integer,
-          :slam_dunks => integer
+         	:player_name => "Jeff Adrien",
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
         }
        ]
      },
